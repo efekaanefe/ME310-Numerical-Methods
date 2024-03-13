6690ef7a4b3d8efb58cb861057dd3d7036ad9f73
efekaanefe <efekaanefe1905@gmail.com>
2024-03-13 22:45:57 +0300

    IQI

--- 2-Root-Finding/inverse_quadratic_interpolation.m
+++ 2-Root-Finding/inverse_quadratic_interpolation.m
@@ -7,7 +7,7 @@ function root = inverse_quadratic_interpolation(func, x0, x1, x2, tol, max_iter)
         y2 = func(x2);
 
         term1 = (y1*y2)/((y0 - y1)*(y0 - y2)) * x0;
-        term2 = (y0*y2)/(y0 - y1 - y2) * x1; % I think there is a typo in eq (6.9) in the book
+        term2 = (y0*y2)/((y1 - y0)* (y1 - y2)) * x1; % I think there is a typo in eq (6.9) in the book
         term3 = (y0*y1)/((y2 - y0)*(y2 - y1))*x2;
         xnew = term1 + term2 + term3;
 
