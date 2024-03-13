9098e41b183264f7f7c72d0014d7bf255e7b7d7c
efekaanefe <efekaanefe1905@gmail.com>
2024-03-13 13:48:02 +0300

    bisection

--- 2-Root-Finding/testing_methods.m
+++ 2-Root-Finding/testing_methods.m
@@ -1,20 +1,26 @@
 clc; clear; close all;
 
-%% global hyper-parameters
+%% hyper-parameters
 
 func = @(x) x^3 - 2*x - 5;
-interval = [0, 10];
+interval = [0, 5];
+delta_x = 0.01;
+tol = 1e-6;
+max_iter = 1000;
+
 %% exact solution and plotting
 
 exact_root = fzero(func, interval);
 % fplot(func, interval)
 
 %% incremental search
-
-delta_x = 0.01;
-roots = incremental_search(func, interval, delta_x);
+roots_incremental_search = incremental_search(func, interval, delta_x);
+root_bisection_method = bisection(func, interval, tol, max_iter);
 
 disp('Exact root:');
 disp(exact_root);
-disp('Potential roots:');
-disp(roots);
+disp('Incremental search:');
+disp(roots_incremental_search);
+disp('Bisection:');
+disp(root_bisection_method);
+
